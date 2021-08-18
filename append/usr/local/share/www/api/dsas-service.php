48725851e042f2b87418f4ab0647a727aaa273f2
David Bateman <david.bateman@edf.fr>
2021-08-18 22:05:30 +0000

    fix: Correctly allow empty ssh user IP addresses, and thus disabling users

--- append/usr/local/share/www/api/dsas-service.php
+++ append/usr/local/share/www/api/dsas-service.php
@@ -16,11 +16,13 @@ else if($_SERVER["REQUEST_METHOD"] == "POST"){
 
         $user_tc = htmlspecialchars(trim($data["ssh"]["user_tc"]));
         $user_tc_err = "";
-        foreach (explode(",",$user_tc) as $inet) {
-          if (substr($inet,0,1) === "!")
-            $inet = substr($inet,1,strlen($inet)-1); 
-          if ($user_tc_err = inet_valid($inet, false))
-            break;
+        if (! empty($user_tc)) {
+          foreach (explode(",",$user_tc) as $inet) {
+            if (substr($inet,0,1) === "!")
+              $inet = substr($inet,1,strlen($inet)-1); 
+            if ($user_tc_err = inet_valid($inet, false))
+              break;
+          }
         }
         if (empty($user_tc_err))
           $dsas->config->ssh->user_tc = $user_tc;
@@ -29,11 +31,13 @@ else if($_SERVER["REQUEST_METHOD"] == "POST"){
 
         $user_bas = htmlspecialchars(trim($data["ssh"]["user_bas"]));
         $user_bas_err = "";
-        foreach (explode(",",$user_bas) as $inet) {
-          if (substr($inet,0,1) === "!")
-            $inet = substr($inet,1,strlen($inet)-1); 
-          if ($user_bas_err = inet_valid($inet, false))
-            break;
+        if (! empty($user_bas)) {
+          foreach (explode(",",$user_bas) as $inet) {
+            if (substr($inet,0,1) === "!")
+              $inet = substr($inet,1,strlen($inet)-1); 
+            if ($user_bas_err = inet_valid($inet, false))
+              break;
+          }
         }
         if (empty($user_bas_err))
           $dsas->config->ssh->user_bas = $user_bas;
@@ -42,11 +46,13 @@ else if($_SERVER["REQUEST_METHOD"] == "POST"){
 
         $user_haut = htmlspecialchars(trim($data["ssh"]["user_haut"]));
         $user_haut_err = "";
-        foreach (explode(",",$user_haut) as $inet) {
-          if (substr($inet,0,1) === "!")
-            $inet = substr($inet,1,strlen($inet)-1); 
-          if ($user_haut_err = inet_valid($inet, false))
-            break;
+        if (! empty($user_haut)) {
+          foreach (explode(",",$user_haut) as $inet) {
+            if (substr($inet,0,1) === "!")
+              $inet = substr($inet,1,strlen($inet)-1); 
+            if ($user_haut_err = inet_valid($inet, false))
+              break;
+          }
         }
         if (empty($user_haut_err))
           $dsas->config->ssh->user_haut = $user_haut;
@@ -57,7 +63,8 @@ else if($_SERVER["REQUEST_METHOD"] == "POST"){
         $dsas->config->ntp->active = ($data["ntp"]["active"] === "true" ? "true" : "false");
 
         $syslog_server = htmlspecialchars(trim($data["syslog"]["server"]));
-        $syslog_err = inet_valid($syslog_server, true);
+        if (! empty($syslog_server))
+          $syslog_err = inet_valid($syslog_server, true);
         if (empty($syslog_err))
           $dsas->config->syslog->server = $syslog_server;
         else
