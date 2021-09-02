cc35d7911a6f3bb746c8d664279294b11ed2a09b
David Bateman <david.bateman@edf.fr>
2021-09-02 06:03:58 +0000

    feat: log acces attempts to the DSAS in syslog

--- append/usr/local/share/www/api/login.php
+++ append/usr/local/share/www/api/login.php
@@ -7,8 +7,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $username = trim($_POST["username"]);
     $password = trim($_POST["password"]);
 
+    $cnxstr = $_SERVER["REMOTE_ADDR"];
+    if (!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
+       $cnxstr = $cnxstr . " [" . $_SERVER["HTTP_X_FORWARDED_FOR"] . "]";
+    else if (!empty($_SERVER["HTTP_CLIENT_IP"]))
+       $cnxstr = $cnxstr . " [" . $_SERVER["HTTP_CLIENT_IP"] . "]";
+
     // Only the user 'tc' is allowed
     if ($username != "tc"){
+        syslog(LOG_WARNING, "Failed DSAS login attempt from " . $cnxstr );
         die(header("HTTP/1.0 403 Forbidden"));
     } else {
         if (sasl_checkpass($username, $password) == 0) {
@@ -25,11 +32,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             session_commit();
             ini_set("session.use_strict_mode", 0);  // Allow new ID
             session_id($newid);
+            syslog(LOG_NOTICE, "Succesful DSAS login from " . $cnxstr);
             echo "Ok";
-        } else
+        } else {
+            syslog(LOG_WARNING, "Failed DSAS login attempt from " . $cnxstr);
             die(header("HTTP/1.0 403 Forbidden"));
+        }
     }
 } else {
+   // No connection attempt so don't log anything to syslog
    if (! dsas_loggedin())
      die(header("HTTP/1.0 403 Forbidden"));
    else
--- append/usr/local/share/www/api/logout.php
+++ append/usr/local/share/www/api/logout.php
@@ -2,14 +2,23 @@
 require_once "common.php";
 
 if (! dsas_loggedin())
+  // No security risk here, trying to logout when not connected. Don't log it
   die(header("HTTP/1.0 403 Forbidden"));
 else {
+  $cnxstr = $_SERVER["REMOTE_ADDR"];
+  if (!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
+     $cnxstr = $cnxstr . " [" . $_SERVER["HTTP_X_FORWARDED_FOR"] . "]";
+  else if (!empty($_SERVER["HTTP_CLIENT_IP"]))
+     $cnxstr = $cnxstr . " [" . $_SERVER["HTTP_CLIENT_IP"] . "]";
+
+
   // Unset all of the session variables
   $_SESSION = array();
 
   // Destory the session
   session_destroy();
 
+  syslog(LOG_NOTICE, "Succesful DSAS logout from " . $cnxstr);
   echo "ok";
 }
 
