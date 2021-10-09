f63aa5a13d0b371e6e6b700a2596cb43795a38f1
David Bateman <david.bateman@edf.fr>
2021-10-09 08:50:04 +0000

    fix: modifiy backup/restore, to exclude passwords and check the tar files for illegal content

--- append/usr/local/share/www/api/backup.php
+++ append/usr/local/share/www/api/backup.php
@@ -8,16 +8,13 @@ else if ($_SERVER["REQUEST_METHOD"] == "POST") {
       // This will throw an error in case of a problem which is caught below 
       check_files($_FILES["file"], "application/gzip");
 
-      // FIXME should control the contents of the file against what is in /opt/.filetool.lst
-      //  and anything not allowed should e rejected. This could reduce the risk of someone
-      // uploading anything they want, but they can already change /etc/shadow, /etc/passwd 
-      // and the configuration of ssh. Frankly if they have admin rights on the web interface
-      // of the DSAS they are alreay on the wrong side of the DSAS..
-
-      $ret = dsas_exec(["/usr/local/sbin/dsasbackup", "-r", $_FILES["file"]["tmp_name"], $_POST["passwd"]]);
+      // The contents of the tar files is controlled in dsasbackup
+      if (empty($_POST["passwd"]))
+        $ret = dsas_exec(["/usr/local/sbin/dsasbackup", "-r", $_FILES["file"]["tmp_name"]]);
+       else
+        $ret = dsas_exec(["/usr/local/sbin/dsasbackup", "-r", $_FILES["file"]["tmp_name"], "-p", $_POST["passwd"]]);
       if ($ret["retval"] != 0)
         throw new RuntimeException("Error pendant la restauration : " . $ret["stderr"]);
-
       echo "Ok";
     }  catch (RuntimeException $e) {
       header("Content-Type: application/json");
@@ -25,7 +22,10 @@ else if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 } else {
   $BKP = "/tmp/dsasbackup.tgz";
-  $ret = dsas_exec(["/usr/local/sbin/dsasbackup", $BKP, $_GET["passwd"]]);
+  if (empty($_GET["passwd"]))
+    $ret = dsas_exec(["/usr/local/sbin/dsasbackup", $BKP]);
+  else
+    $ret = dsas_exec(["/usr/local/sbin/dsasbackup", $BKP, "-p", $_GET["passwd"]]);
   if ($ret["retval"] != 0)
     die(header("HTTP/1.0 500 Internal Server Error: " . $ret["stderr"]));
   else {
