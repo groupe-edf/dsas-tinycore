23eb31f66cbc8563ee858c946e6d2616c1f2dc6a
David Bateman <David.Bateman@edf.fr>
2021-10-22 17:21:24 +0000

    feat: Make /var/log persistent

--- append/opt/bootsync.sh
+++ append/opt/bootsync.sh
@@ -2,7 +2,6 @@
 # put other system startup commands here, the boot process will wait until they complete.
 # Use bootlocal.sh for system startup commands that can run in the background 
 # and therefore not slow down the boot process.
-/usr/bin/sethostname box
 /etc/init.d/services/dsas start
 /usr/local/sbin/saslauthd -a shadow &
 /opt/bootlocal.sh &
