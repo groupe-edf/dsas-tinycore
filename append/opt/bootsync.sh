523da824f3291cfa0049705b1c598dbc7affd617
David Bateman <David.Bateman@edf.fr>
2021-12-13 12:39:13 +0000

    feat: start clamav from dsas service. Add crontab for clamav signature updates

--- append/opt/bootsync.sh
+++ append/opt/bootsync.sh
@@ -3,4 +3,5 @@
 # Use bootlocal.sh for system startup commands that can run in the background 
 # and therefore not slow down the boot process.
 /etc/init.d/services/dsas start
+/usr/local/sbin/saslauthd -a shadow &
 /opt/bootlocal.sh &
