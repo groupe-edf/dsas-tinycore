3cc6770e68ef3f4378d965a9378cc7fbf10b52ed
David Bateman <David.Bateman@edf.fr>
2021-12-13 12:39:13 +0000

    feat: start clamav from dsas service. Add crontab for clamav signature updates

--- append/opt/bootlocal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-/usr/local/sbin/saslauthd -a shadow &
-
-if [ -n "/home/dsas/bas/share/ClamAV" ]; then
-  mkdir -p /home/dsas/bas/share/ClamAV
-  chown verif.share /home/dsas/dsas/share/ClamAV
-fi
-sudo -u verif /usr/local/sbin/clamd &
