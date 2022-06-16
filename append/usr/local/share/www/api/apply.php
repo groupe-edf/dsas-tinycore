ce289672a06bd65bfb8061dc67a56b73a37d5e0d
David Bateman <David.Bateman@edf.fr>
2022-06-16 14:26:18 +0000

    fix: error applying after file permission changes

--- append/usr/local/share/www/api/apply.php
+++ append/usr/local/share/www/api/apply.php
@@ -15,14 +15,18 @@ else {
 
   if ($output["retval"] == 0) 
     $output = dsas_exec(["sudo", "sudo", "-u", "haut", "ssh", "tc@" . $haut, "cp", "/var/dsas/dsas_conf.xml", "/var/dsas/dsas_conf.xml.old"]);
-  if ($output["retval"] == 0) 
-    $output = dsas_exec(["sudo", "sudo", "-u", "haut", "scp", "/var/dsas/dsas_conf.xml", "tc@" . $haut . ":/tmp"]);
+  if ($output["retval"] == 0) {
+    copy("/var/dsas/dsas_conf.xml", "/tmp/dsas_conf.xml");
+    chmod("/tmp/dsas_conf.xml", 644);
+    $output = dsas_exec(["sudo", "sudo", "-u", "haut", "scp", "/tmp/dsas_conf.xml", "tc@" . $haut . ":/tmp"]);
+    unlink("/tmp/dsas_conf.xml");
+  }
   if ($output["retval"] == 0) 
     $output = dsas_exec(["sudo", "sudo", "-u", "haut", "ssh", "tc@" . $haut, "mv", "/tmp/dsas_conf.xml", "/var/dsas/dsas_conf.xml"]);
   if ($output["retval"] == 0)
     $output = dsas_exec(["sudo", "sudo", "-u", "haut", "ssh", "tc@" . $haut, "sudo", "/etc/init.d/services/dsas", "apply"]);
   if ($output["retval"] != 0)
-    header("HTTP/1.0 500 Internal Server Error: " . $output["stderr"]);
+    header("HTTP/1.0 500 Internal Server Error: " . explode("/n", $output["stderr"])[0]);
   else
     echo "Ok";
 }
