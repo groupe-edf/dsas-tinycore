40c68d076065a6848cfe6aba69fa3c44e1f18504
David Bateman <David.Bateman@edf.fr>
2022-09-16 20:11:41 +0000

    fix: properly seperate the presentation et javascript of net.html

--- js/src/DsasNet.js
+++ js/src/DsasNet.js
@@ -1,4 +1,5 @@
 // The javascript used by the DSAS service.html page
+use strict;
 import { _ } from "./MultiLang";
 import { modal_message, modal_errors } from "./DsasModal";
 import {
@@ -8,97 +9,7 @@ import {
     empty_obj,
 } from "./DsasUtil";
 
-function iface_body(iface, i) {
-    const dhcp = (iface.dhcp === "true");
-
-    return "<div class=\"row\">\n"
-        + "  <div class=\"col-6\">\n"
-        + "    <div class=\"row\">\n"
-        + "      <div class=\"col-12\">\n"
-        + "        <input class=\"form-check-input\" type=\"checkbox\"" + (dhcp ? " checked=\"\"" : "") + " id=\"iface_dhcp" + i
-        + "\" onchange=\"dsas_change_net('dhcp', " + i + ");\">\n"
-        + "        <label class=\"form-check-label\" for=\"iface_dhcp" + i + "\">" + _("Use DHCP") + "</label>\n"
-        + "      </div>\n"
-        + "      <div class=\"col-12\">\n"
-        + "        <label for=\"iface_cidr" + i + "\">" + _("IP address and mask (CIDR format)") + "</label>\n"
-        + "        <input type=\"text\" id=\"iface_cidr" + i + "\"" + (dhcp ? " disabled=\"\"" : "") + " value=\"" + print_obj(iface.cidr) + "\" class=\"form-control\">\n"
-        + "        <div class=\"invalid-feedback\" id=\"feed_iface_cidr" + i + "\"></div>\n"
-        + "      </div>\n"
-        + "      <div class=\"col-12\">\n"
-        + "        <label for=\"iface_gateway" + i + "\">" + _("Gateway") + "</label>\n"
-        + "        <input type=\"text\" id=\"iface_gateway" + i + "\"" + (dhcp ? " disabled=\"\"" : "") + " value=\"" + print_obj(iface.gateway) + "\" class=\"form-control\">\n"
-        + "        <div class=\"invalid-feedback\" id=\"feed_iface_gateway" + i + "\"></div>\n"
-        + "      </div>\n"
-        + "    </div>\n"
-        + "  </div>\n"
-        + "  <div class=\"col-6\">\n"
-        + "    <div class=\"row\">\n"
-        + "      <div class=\"col-12\">\n"
-        + "        <label for=\"iface_dns_domain" + i + "\">" + _("DNS domain") + "</label>\n"
-        + "        <input type=\"text\" id=\"iface_dns_domain" + i + "\"" + (dhcp ? " disabled=\"\"" : "") + " value=\"" + print_obj(iface.dns.domain) + "\" class=\"form-control\">\n"
-        + "        <div class=\"invalid-feedback\" id=\"feed_iface_dns_domain" + i + "\"></div>\n"
-        + "      </div>\n"
-        + "      <div class=\"form-check col-12\">\n"
-        + "        <label for=\"iface_nameserver" + i + "\">" + _("DNS name servers") + "</label>\n"
-        + "        <textarea name=\"iface_nameserver" + i + "\"" + (dhcp ? " disabled=\"\"" : "") + " rows=\"3\" id=\"iface_nameserver" + i + "\" class=\"form-control\"></textarea>\n"
-        + "        <div class=\"invalid-feedback\" id=\"feed_iface_nameserver" + i + "\"></div>\n"
-        + "      </div>\n"
-        + "    </div>\n"
-        + "  </div>\n"
-        + "</div>";
-}
-
-export default function dsas_display_net(what = "all") {
-    fetch("api/dsas-net.php").then((response) => {
-        if (response.ok) { return response.json(); }
-        return Promise.reject(new Error(response.statusText));
-    }).then((net) => {
-        if (what === "ifaces" || what === "all") {
-            let i = 0;
-            let iface;
-            let body = "<h5>" + _("Network configuration") + "</h5>\n"
-                + "<form class=\"row g-2\">\n"
-                + "  <div class=\"container p-3 my-3 border\" id=\"IFaces\">";
-            ["bas", "haut"].forEach((iface2) => {
-                iface = net[iface2];
-                body += "<p class=\"my-0\"><a class=\"text-toggle\" data-bs-toggle=\"collapse\" href=\"#iface" + i
-                    + "\" role=\"button\" aria-controls=\"iface" + i + "\" aria-expanded=\"false\">"
-                    + "<i class=\"text-collapsed\"><img src=\"caret-right.svg\"/></i>"
-                    + "<i class=\"text-expanded\"><img src=\"caret-down.svg\"/></i></a>"
-                    + (iface2 === "bas" ? _("Lower Machine") : _("Upper Machine"));
-                body += "</p><div class=\"collapse\" id=\"iface" + i + "\"><div class=\"card card-body\">"
-                    + iface_body(iface, i) + "</div></div>\n";
-                i += 1;
-            });
-            body += "</div><div class=\"form-group\">\n"
-                + "  <input type=\"submit\" class=\"btn btn-primary\" value=\"" + _("Save the changes") + "\" onclick=\"dsas_change_net(); return false;\">\n"
-                + "</div></form>";
-
-            document.getElementById("network").innerHTML = body;
-
-            // Why can't I include this in the declaration of the body ?
-            i = 0;
-            [net.bas, net.haut].forEach((iface2) => {
-                let dns_servers = "";
-                if (!empty_obj(iface2.dns.nameserver)) {
-                    (iface2.dns.nameserver.constructor === Array
-                        ? iface2.dns.nameserver : [iface2.dns.nameserver]).forEach((ns) => {
-                        dns_servers += ns + "\n";
-                    });
-                }
-                document.getElementById("iface_nameserver" + i).value = dns_servers;
-                i += 1;
-            });
-        }
-    }).catch((error) => {
-        if (!fail_loggedin(error)) {
-            modal_message(_("Error while loading the page : {0}", (error.message ? error.message : error)));
-        }
-    });
-}
-window.dsas_display_net = dsas_display_net;
-
-export function dsas_change_net(what = "all", i = 0) {
+function dsas_change_net(what = "all", i = 0) {
     if (what === "dhcp") {
         const chk = document.getElementById("iface_dhcp" + i).checked;
         document.getElementById("iface_cidr" + i).disabled = chk;
@@ -139,7 +50,8 @@ export function dsas_change_net(what = "all", i = 0) {
                 } catch (e) {
                     // Its text => here always just "Ok"
                     clear_feedback();
-                    dsas_display_net("all");
+                    /* eslint-disable-next-line no-use-before-define */
+                    dsas_display_net("ifaces");
                 }
             }).catch((error) => {
                 modal_message(_("Error while loading the page : {0}", (error.message ? error.message : error)));
@@ -149,4 +61,44 @@ export function dsas_change_net(what = "all", i = 0) {
         });
     }
 }
-window.dsas_change_net = dsas_change_net;
+
+export default function dsas_display_net(what = "all") {
+    fetch("api/dsas-net.php").then((response) => {
+        if (response.ok) { return response.json(); }
+        return Promise.reject(new Error(response.statusText));
+    }).then((net) => {
+        let i = 0;
+        ["bas", "haut"].forEach((iface2) => {
+            const iface = net[iface2];
+            const dhcp = (iface.dhcp === "true");
+            let dns_servers = "";
+            document.getElementById("iface_dhcp" + i).setAttribute("checked", "");
+            if (what === "all") {
+                const fn = function (y) { return function () { dsas_change_net("dhcp", j); }}(i);
+                document.getElementById("iface_dhcp" + i).addEventListener("change", fn);
+            }
+            document.getElementById("iface_cidr" + i).disabled = dhcp;
+            document.getElementById("iface_cidr" + i).value = print_obj(iface.cidr);
+            document.getElementById("iface_gateway" + i).disabled = dhcp;
+            document.getElementById("iface_gateway" + i).value = print_obj(iface.gateway);
+            document.getElementById("iface_dns_domain" + i).disabled = dhcp;
+            document.getElementById("iface_dns_domain" + i).value = print_obj(iface.dns.domain);
+            document.getElementById("iface_nameserver" + i).disabled = dhcp;
+            if (!empty_obj(iface.dns.nameserver)) {
+                (iface.dns.nameserver.constructor === Array
+                    ? iface.dns.nameserver : [iface.dns.nameserver]).forEach((ns) => {
+                    dns_servers += ns + "\n";
+                });
+            }
+            document.getElementById("iface_nameserver" + i).value = dns_servers;
+            i += 1;
+        });
+        if (what === "all") {
+            document.getElementById("netsubmit").addEventListener("click", () => { dsas_change_net(); });
+        }
+    }).catch((error) => {
+        if (!fail_loggedin(error)) {
+            modal_message(_("Error while loading the page : {0}", (error.message ? error.message : error)));
+        }
+    });
+}
