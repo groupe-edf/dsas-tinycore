ba50a175b63a3cb3273b6ee9010f6dec7e6c834d
David Bateman <David.Bateman@edf.fr>
2022-10-03 17:01:26 +0000

    docs: bump version to 1.2.3

--- js/src/DsasHelp.js
+++ js/src/DsasHelp.js
@@ -9,7 +9,7 @@ import { modalMessage } from "./DsasModal";
 import { failLoggedin, dsasOrigin } from "./DsasUtil";
 
 // DSAS version variable
-const dsasVersion = "1.2.2";
+const dsasVersion = "1.2.3";
 
 function dsasHeadings() {
     const hs = Array.prototype.slice.call(document.querySelectorAll("h1, h2, h3"));
