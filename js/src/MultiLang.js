d2e32ba8379b58e479c534aae84595b5eb8e7a38
David Bateman <David.Bateman@edf.fr>
2022-10-04 13:19:47 +0000

    docs: Apply GPLv2 licence

--- js/src/MultiLang.js
+++ js/src/MultiLang.js
@@ -1,3 +1,20 @@
+//    DSAS - Tinycore
+//    Copyright (C) 2021-2022  Electricite de France
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 // A Class to support multiple languages on webpages and automatically translate
 // elements tagged with data-i18n*. Also used to translate in JavaScript via
 // MultiLang.translate and to treat translates of formatted strings
