13601d325772d14de5347f1f357245efc0d2c507
David Bateman <David.Bateman@edf.fr>
2022-10-13 09:18:00 +0000

    feat: kernel option hardening. needs testing (suggestion lynis)

--- append/opt/bootsync.sh
+++ append/opt/bootsync.sh
@@ -2,5 +2,6 @@
 # put other system startup commands here, the boot process will wait until they complete.
 # Use bootlocal.sh for system startup commands that can run in the background 
 # and therefore not slow down the boot process.
+sysctl -p /etc/sysctl.conf
 /etc/init.d/services/dsas start
 /opt/bootlocal.sh &
