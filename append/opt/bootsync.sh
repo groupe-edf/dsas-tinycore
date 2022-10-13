b4b27d75704c819872b6a2219ca0ceff3eac99b2
David Bateman <David.Bateman@edf.fr>
2022-10-13 14:20:03 +0000

    fix: sysctl less verbose

--- append/opt/bootsync.sh
+++ append/opt/bootsync.sh
@@ -2,6 +2,6 @@
 # put other system startup commands here, the boot process will wait until they complete.
 # Use bootlocal.sh for system startup commands that can run in the background 
 # and therefore not slow down the boot process.
-sysctl -p /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf > /dev/null 2>&1
 /etc/init.d/services/dsas start
 /opt/bootlocal.sh &
